86c49d9ae4afeb580a2f1d757eedf5a6ebee9c60
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-04-01 17:59:33 -0400

    created five main CRUD methods for the /api/users endpoint routes.

---
+++ controllers/username-controller.js
@@ -0,0 +1,65 @@
+const { process_params } = require('express/lib/router');
+const { Username } = require('../models');
+
+const usernameController = {
+    //get all users
+    getAllUsers(req, res) {
+        Username.find({})
+            .then(dbUsernameData => res.json(dbUsernameData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    //get a single user by their _id and populated thought and friend data
+    getUserById(req, res) {
+        Username.findOne({ _id: process_params.id })
+            .then(dbUsernameData => {
+                //if no user is found, send 404
+                if (!dbusernameData) {
+                    res.status(404).json({ message: 'No User found with this Id' })
+                    return;
+                }
+                res.json(dbUsernameData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    createUser({ body }, res) {
+        Username.create(body)
+            .then(dbUsernameData => res.json(dbUsernameData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    //Update a user by their _id
+    updateUser({ params, body }, res) {
+        Username.findOneAndUpdate({ params: params.id }, body, { new: true })
+            .then(dbUsernameData => {
+                if (!dbUsernameData) {
+                    res.status(404).json({ message: 'No user found with this Id' })
+                    return;
+                }
+                res.json(dbUsernameData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    //Delete a user by their _id
+    deleteUser({ params }, res) {
+        Username.findOneAndDelete({ _id: params.id })
+        .then(dbUsernameData => {
+            if (!dbUsernameData) {
+                res.status(404).json({ message: 'No User found with this Id'})
+                return;
+            }
+            res.json(dbUsernameData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = usernameController;
